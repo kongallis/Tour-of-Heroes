878877ecab72e2b641db06e6aaa2d5c563fdf709
kongallis <kongallis@gmail.com>
2020-11-10 21:47:14 +0200

    Create service to fetch heroes synchronously

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { HeroesComponent } from "./heroes/heroes.component";
+import { HeroService } from "./hero.service";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeroesComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, HeroesComponent],
+  bootstrap: [AppComponent],
+  providers: [HeroService]
 })
-export class AppModule { }
+export class AppModule {}
