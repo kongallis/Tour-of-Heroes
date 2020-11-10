e73d41058181388504e2d5211235a521cc0e96b0
kongallis <kongallis@gmail.com>
2020-11-10 21:54:54 +0200

    Asynchronous heroes fetching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { HeroService } from "./hero.service";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, HeroesComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeroesComponent,
+    HeroDetailComponent
+  ],
   bootstrap: [AppComponent],
   providers: [HeroService]
 })
