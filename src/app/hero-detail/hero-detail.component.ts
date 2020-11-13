30aa6442ddcadc611cf9b68caaff1ae4e454cfef
kongallis <kongallis@gmail.com>
2020-11-13 14:16:42 +0200

    Extract information from the URL

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { Hero } from "../hero";
+import { ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
+
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: "app-hero-detail",
@@ -9,7 +13,21 @@ import { Hero } from "../hero";
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
 
-  constructor() {}
+  constructor(
+    // holds info about the route of this instance(e.g. id)
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getHero();
+  }
+
+  getHero(): void {
+    // Route parameters are always strings
+    // + : converts the string to a number
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
+  }
 }
